25a09b0d3acbaf56124c560e4c775e4775d8b0bd
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-07-29 10:33:55 +0200

    ejercicio22

---
+++ ejercicio22/ejercicio22.py
@@ -0,0 +1,18 @@
+#Determinar si un número X es perfecto,
+#un número es perfecto si la suma de sus divisores es el
+#mismo número. Ej. El 6 es perfecto ya que 1+2+3=6
+
+def numeroPerfecto(num):
+    
+    suma=0;
+    for i in range(1,num):
+        
+        if((num%i==0)or(i==num)):
+            print(i)
+            suma+=i;
+    if(suma==num):
+        print(f"{num} es un número perfecto ya que la suma de sus divisores es {suma}")
+    else:
+        print(f"{num} no es un número perfecto; ya que la suma de sus divisores es {suma}")
+
+numeroPerfecto(6)
