e63205e3cc793d5a272bbd5e217e69731d4bd79b
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-08-07 20:05:30 +0200

    cotizacionBolsa ejercicio39

--- cotizacionBolsa/ejercicio39.py
+++ cotizacionBolsa/ejercicio39.py
@@ -4,7 +4,7 @@ El fichero cotizacion.csv contiene las cotizaciones de las empresas del IBEX35 c
 nombre (nombre de la empresa), Final (precio de la acción al cierre de bolsa), Máximo (precio máximo de la 
 acción durante la jornada), Mínimo (precio mínimo de la acción durante la jornada), volumen (Volumen al cierre de bolsa),
 Efectivo (capitalización al cierre en miles de euros). Construir una función que construya un DataFrame a partir del un 
-fichero con el formato anterior y devuelva otro DataFrame con el mínimo, el máximo y la media de dada columna.
+fichero con el formato anterior y devuelva otro DataFrame con el mínimo, el máximo y la media de cada columna.
 
 """
 import pandas as pd
