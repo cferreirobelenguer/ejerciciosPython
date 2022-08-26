476e7d25b9ee52c22c014ab945083f0a2ac95276
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-08-27 01:36:49 +0200

    eliminarCaracteres ejercicio 55

--- eliminarCaracteres/ejercicio55.py
+++ eliminarCaracteres/ejercicio55.py
@@ -3,26 +3,30 @@ Enunciado: Crea dos funciones que reciban dos cadenas como parámetro (str1, str
     - out1 contendrá todos los caracteres presentes en la str1 pero NO estén presentes en str2.
     - out2 contendrá todos los caracteres presentes en la str2 pero NO estén presentes en str1
 """
-str1="carmelahy"
-str2="sarayuee"
+str1="carmela y pepaz"
+str2="sara y pepita"
 
 def calcularOUT1(str1,str2):
     out1=""
     lista=[]
+    str1=str1.lower()
     for i in str2:
-        lista.append(i)
+        if(i!=" "):
+            lista.append(i)
     for j in str1:
-        if j not in lista:
+        if ((j not in lista)and(j!=" ")):
             out1+=j
     
     return out1
 def calcularOUT2(str1,str2):
     out2=""
+    str2=str2.lower()
     lista=[]
     for i in str1:
-        lista.append(i)
+        if(i!=" "):
+            lista.append(i)
     for j in str2:
-        if j not in lista:
+        if ((j not in lista)and(j!=" ")):
             out2+=j
     return out2
 print(calcularOUT1(str1,str2))
